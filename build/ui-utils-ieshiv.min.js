86c588d120629af5b837e8e3ebfa627d35693679
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2013-12-24 16:07:17 +0000

    Travis commit : build 94

--- build/ui-utils-ieshiv.min.js
+++ build/ui-utils-ieshiv.min.js
@@ -1,6 +1,6 @@
 /**
  * angular-ui-utils - Swiss-Army-Knife of AngularJS tools (with no external dependencies!)
- * @version v0.0.4 - 2013-12-19
+ * @version v0.0.4 - 2013-12-24
  * @link http://angular-ui.github.com
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
