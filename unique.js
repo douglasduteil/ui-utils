c32a02b10fe378840b785c11857c85c01ff1df0e
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2013-12-26 16:43:40 +0000

    Travis commit : build 104

--- unique.js
+++ unique.js
@@ -1,3 +1,4 @@
+'use strict';
 angular.module('ui.unique', []).filter('unique', [
   '$parse',
   function ($parse) {
@@ -6,14 +7,14 @@ angular.module('ui.unique', []).filter('unique', [
         return items;
       }
       if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
-        var hashCheck = {}, newItems = [], get = angular.isString(filterOn) ? $parse(filterOn) : function (item) {
+        var newItems = [], get = angular.isString(filterOn) ? $parse(filterOn) : function (item) {
             return item;
           };
         var extractValueToCompare = function (item) {
           return angular.isObject(item) ? get(item) : item;
         };
         angular.forEach(items, function (item) {
-          var valueToCheck, isDuplicate = false;
+          var isDuplicate = false;
           for (var i = 0; i < newItems.length; i++) {
             if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
               isDuplicate = true;
