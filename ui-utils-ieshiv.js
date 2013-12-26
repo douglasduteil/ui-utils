b15aed78ab76fad2777cfd13ce9669d297c91f82
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2013-12-26 16:43:06 +0000

    Travis commit : build 104

--- ui-utils-ieshiv.js
+++ ui-utils-ieshiv.js
@@ -1,6 +1,6 @@
 /**
  * angular-ui-utils - Swiss-Army-Knife of AngularJS tools (with no external dependencies!)
- * @version v0.0.4 - 2013-12-25
+ * @version v0.0.4 - 2013-12-26
  * @link http://angular-ui.github.com
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
@@ -19,9 +19,9 @@
 // <![endif]-->
 
 (function (window, document) {
+  "use strict";
 
-  var debug = window.ieShivDebug || false,
-      tags = [ "ngInclude", "ngPluralize", "ngView", "ngSwitch", "uiCurrency", "uiCodemirror", "uiDate", "uiEvent",
+  var tags = [ "ngInclude", "ngPluralize", "ngView", "ngSwitch", "uiCurrency", "uiCodemirror", "uiDate", "uiEvent",
                 "uiKeypress", "uiKeyup", "uiKeydown", "uiMask", "uiMapInfoWindow", "uiMapMarker", "uiMapPolyline",
                 "uiMapPolygon", "uiMapRectangle", "uiMapCircle", "uiMapGroundOverlay", "uiModal", "uiReset",
                 "uiScrollfix", "uiSelect2", "uiShow", "uiHide", "uiToggle", "uiSortable", "uiTinymce"
@@ -35,7 +35,7 @@
     var dashed = str.replace(/([A-Z])/g, function ($1) {
       return " " + $1.toLowerCase();
     });
-    var tokens = dashed.split(' ');
+    var tokens = dashed.split(" ");
 
     // If a token is just a single name (i.e. no namespace) then we juse define the elements the name given
     if (tokens.length === 1) {
@@ -46,7 +46,7 @@
       result.push("data-" + name);
     } else {
       var ns = tokens[0];
-      var dirname = tokens.slice(1).join('-');
+      var dirname = tokens.slice(1).join("-");
 
       // this is finite list and it seemed senseless to create a custom method
       result.push(ns + ":" + dirname);
